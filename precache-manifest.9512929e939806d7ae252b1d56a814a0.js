b804f9d30b951bc5b4b8839c718bb38c77363ca7
dronaka <lucevar@yandex.ru>
2019-12-05 19:01:28 +0300

    add gh-page lab2

---
+++ precache-manifest.9512929e939806d7ae252b1d56a814a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5815b982eb3a06ec65bcaf166d7b99a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ceb2df9cbd8b947016d",
+    "url": "/static/css/main.cca60f5b.chunk.css"
+  },
+  {
+    "revision": "71f917fa29012cf8b657",
+    "url": "/static/js/2.9b6ac575.chunk.js"
+  },
+  {
+    "revision": "5ceb2df9cbd8b947016d",
+    "url": "/static/js/main.ffe57baa.chunk.js"
+  },
+  {
+    "revision": "df86a4573f13849561be",
+    "url": "/static/js/runtime-main.f105a251.js"
+  }
+]);
